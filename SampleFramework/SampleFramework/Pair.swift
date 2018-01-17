d010ca07c0ee2f94f5e655bc4ae7b695867156f3
Zeke17 <nems3d@hotmail.fr>
2018-01-17 15:07:10 +0100

    first commit

---
+++ SampleFramework/SampleFramework/Pair.swift
@@ -0,0 +1,15 @@
+//
+//  Pair.swift
+//  SampleFramework
+//
+//  Created by Nam Nguyen on 17/01/2018.
+//  Copyright © 2018 Nam Nguyen. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    public var isPair: Bool {
+        return self % 2 == 0
+    }
+}
---
+++ SampleFramework/SampleFrameworkTests/SampleFrameworkTests.swift
@@ -0,0 +1,38 @@
+//
+//  SampleFrameworkTests.swift
+//  SampleFrameworkTests
+//
+//  Created by Nam Nguyen on 17/01/2018.
+//  Copyright © 2018 Nam Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import SampleFramework
+
+class SampleFrameworkTests: XCTestCase {
+    
+    func testZero() {
+        XCTAssertEqual(0.isPair, true)
+    }
+    
+    func test1() {
+        XCTAssertEqual(1.isPair, false)
+    }
+    
+    func test2() {
+        XCTAssertEqual(2.isPair, true)
+    }
+    
+    func test440() {
+        XCTAssertEqual(440.isPair, true)
+    }
+    
+    func testNeg1() {
+        XCTAssertEqual((-1).isPair, false)
+    }
+    
+    func testNeg2() {
+        XCTAssertEqual((-2).isPair, true)
+    }
+    
+}
